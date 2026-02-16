9e732d289df88213154f0755ea558c6122d30d07
AstralStew <pinky@paperticketstudios.com>
2026-02-16 17:31:21 +1100

    Added enemies

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -7,6 +7,7 @@ var player : Node2D = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_tree().get_nodes_in_group("Player")[0]
+	reset()
 
 
 func _physics_process(delta: float) -> void:
@@ -17,4 +18,12 @@ func find_player(delta:float) -> void:
 	
 	var direction : Vector2 = (player.global_position - global_position).normalized()
 	velocity = direction * speed
-	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("body = ", body)
+	if body == player:
+		reset()
+
+func reset() -> void:
+	global_position = Vector2( randf_range(-100,1200),randf_range(-100,1000) )
